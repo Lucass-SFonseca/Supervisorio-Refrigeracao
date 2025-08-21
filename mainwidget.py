febd6628d1dfc04d0631773616a52acf7ab98233
DuddaAraujo <duddsdearaujo@gmail.com>
2025-08-20 22:07:08 -0300

    .

--- mainwidget.py
+++ mainwidget.py
@@ -60,6 +60,7 @@ class MainWidget(BoxLayout):
         print("Tags disponíveis no sistema:")
         for key in self._tags.keys():
             print(f"  - {key}")
+    
     def startDataRead(self, ip, port):
         self._serverIP = ip
         self._serverPort = port
